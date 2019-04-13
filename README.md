f1c95a32ba213c5a8c2ebe4d5dc343d2ffcb53e3
Hugo Meyronneinc <hugo@yoursoft.run>
2019-04-13 15:11:53 +0200

    Module config tests

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![Build Status](https://travis-ci.org/YourSoftRun/yet-another-nuxt-oauth2.svg?branch=master)](https://travis-ci.org/YourSoftRun/yet-another-nuxt-oauth2)
 [![Coverage Status](https://coveralls.io/repos/github/YourSoftRun/yet-another-nuxt-oauth2/badge.svg?branch=master)](https://coveralls.io/github/YourSoftRun/yet-another-nuxt-oauth2?branch=master)
-[![Codacy Badge](https://api.codacy.com/project/badge/Grade/cb024436e1a84097b81775692d38ca8a)](https://www.codacy.com/app/Hugome/yet-another-nuxt-oauth2?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=YourSoftRun/yet-another-nuxt-oauth2&amp;utm_campaign=Badge_Grade)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/683d1198ec0e4c50a2b0c58174244d5e)](https://www.codacy.com/app/Hugome/yet-another-nuxt-oauth2?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=YourSoftRun/yet-another-nuxt-oauth2&amp;utm_campaign=Badge_Grade)
 [![Maintainability](https://api.codeclimate.com/v1/badges/2f19318b8b78081f0506/maintainability)](https://codeclimate.com/github/YourSoftRun/yet-another-nuxt-oauth2/maintainability)
 [![David](https://img.shields.io/david/YourSoftRun/yet-another-nuxt-oauth2.svg)](https://david-dm.org/YourSoftRun/yet-another-nuxt-oauth2)
 [![Known Vulnerabilities](https://snyk.io/test/github/YourSoftRun/yet-another-nuxt-oauth2/badge.svg)](https://snyk.io/test/github/YourSoftRun/yet-another-nuxt-oauth2)
@@ -10,7 +10,36 @@
 [![Downloads](https://img.shields.io/npm/dm/yet-another-nuxt-oauth2.svg)](https://www.npmjs.com/package/yet-another-nuxt-oauth2)
 [![FOSSA Status](https://app.fossa.io/api/projects/git%2Bgithub.com%2FYourSoftRun%2Fyet-another-nuxt-oauth2.svg?type=shield)](https://app.fossa.io/projects/git%2Bgithub.com%2FYourSoftRun%2Fyet-another-nuxt-oauth2?ref=badge_shield)
 
+## Features
+
+- Configuration by object or `.well-know/openid-configuration`
+- Full SSR compatible
+- Expiration/Refresh token
+
+### Grant type support
+
+|  Grant              |   |
+|---------------------|---|
+| Authorization code  | ✓ |
+| Implicit            | ✓ |
+| Password            | ✓ |
+| Device code         |   |
+
+## Module options
+```js
+{
+  endpoints: {
+    authorization_endpoint: 'http://auth.server/authorization',
+    token_endpoint: 'http://auth.server/token'
+    userinfo_endpoint: 'http://auth.server/userinfo'
+  }, // Object or 'http://auth.my.server/.well-know/openid-configuration'
+  router: false // Auto add auth middleware to the router
+}
+```
+
 ## How to use it
+If you want a great open source auth/IAM server : https://www.keycloak.org/ (This one is :fireworks:)
 ```js
 // TODO
 ```
+
--- test/unit/module.spec.js
+++ test/unit/module.spec.js
@@ -1,22 +1,88 @@
+/* eslint-disable no-console */
+const fetchMock = require('fetch-mock')
 const { resolve } = require('path')
 const { Nuxt, Builder } = require('nuxt')
 
+
 describe('Module config', () => {
-  let nuxt = null
+  jest.setTimeout(30000) // Maybe find to do a faster nuxt build ?
 
-  beforeAll(async () => {
-    const config = {
-      dev: false,
-      rootDir: resolve(__dirname, '..')
-    }
-    nuxt = new Nuxt(config)
-    await new Builder(nuxt).build()
-    await nuxt.server.listen(4000, 'localhost')
+  const buildNuxt = async config => {
+    return new Builder(new Nuxt({
+      dev: true,
+      rootDir: resolve(__dirname, '..', 'fixtures', 'module'),
+      modules: [resolve(__dirname, '..', '..', 'lib', 'module.js')],
+      ...config
+    })).build()
+  }
+
+  it('should not init yano (Missing config)', async () => {
+    console.warn = jest.fn()
+    await buildNuxt()
+    expect(console.warn).toHaveBeenCalledTimes(1)
+    expect(console.warn).toHaveBeenCalledWith('[YANO] No oauth2 configuration endpoint (Or configuration object)')
   })
-  afterAll(() => nuxt.close())
 
-  it('should not init sentry', async () => {
-    console.log(nuxt)
+  it('should not init yano (With wrong inline object config)', async () => {
+    console.warn = jest.fn()
+    await buildNuxt({
+      yano: {
+        endpoints: {
+          missing_endpoint: 'not'
+        }
+      }
+    })
+    expect(console.warn).toHaveBeenCalledTimes(1)
+    expect(console.warn).toHaveBeenCalledWith('[YANO] Wrong oauth2 configuration (Mandatory : authorization_endpoint, token_endpoint, userinfo_endpoint or unsupported grant type)')
   })
 
+  it('should not init yano (With unsupported grant - via http autoconfigure)', async () => {
+    console.warn = jest.fn()
+    fetchMock.get('http://this.is.not.a.server/auth', { authorization_endpoint: 'http://authorization', token_endpoint: 'http://token', userinfo_endpoint: 'http://userinfo', grant_types_supported: ['password'] })
+    await buildNuxt({
+      yano: {
+        endpoints: 'http://this.is.not.a.server/auth',
+        grant: 'implicit'
+      }
+    })
+    expect(console.warn).toHaveBeenCalledTimes(1)
+    expect(console.warn).toHaveBeenCalledWith('[YANO] Wrong oauth2 configuration (Mandatory : authorization_endpoint, token_endpoint, userinfo_endpoint or unsupported grant type)')
+  })
+
+  it('should init yano', async () => {
+    console.warn = jest.fn()
+    const { plugins } = await buildNuxt({
+      yano: {
+        endpoints: {
+          authorization_endpoint: 'http://authorization',
+          token_endpoint: 'http://token',
+          userinfo_endpoint: 'http://userinfo'
+        },
+        grant: 'implicit'
+      }
+    })
+    expect(console.warn).not.toHaveBeenCalledTimes(1)
+    expect(plugins.length).toBe(1)
+    expect(plugins[0].src).toContain('middlewares')
+  })
+
+  it('should init yano with auto router', async () => {
+    console.warn = jest.fn()
+    const { nuxt } = await buildNuxt({
+      yano: {
+        endpoints: {
+          authorization_endpoint: 'http://authorization',
+          token_endpoint: 'http://token',
+          userinfo_endpoint: 'http://userinfo'
+        },
+        grant: 'implicit',
+        router: true
+      }
+    })
+    expect(console.warn).not.toHaveBeenCalledTimes(1)
+    expect(nuxt.options.router).toBeDefined()
+    expect(nuxt.options.router.middleware).toBeDefined()
+    expect(nuxt.options.router.middleware.length).toBe(1)
+    expect(nuxt.options.router.middleware[0]).toBe('auth')
+  })
 })
